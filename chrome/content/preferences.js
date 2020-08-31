1cec48084f2d6c88b574dac03f13dbb1cf4ea337
sagudev <samo.golez@outlook.com>
2020-08-31 09:25:42 +0200

    refacto, prep for work

--- chrome/content/preferences.js
+++
@@ -1,5 +0,0 @@
-Preferences.addAll([
-    { id: "extensions.encryptifpossible.auto_encrypt_ask", type: "bool" },
-    { id: "extensions.encryptifpossible.auto_sign", type: "bool" },
-    { id: "extensions.encryptifpossible.warn_no_encrypt", type: "int" },
-]);
--- defaults/preferences/defaults.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.encryptifpossible.debug", false);
-pref("extensions.encryptifpossible.auto_encrypt_ask", true);
-pref("extensions.encryptifpossible.warn_no_encrypt", 0);
-pref("extensions.encryptifpossible.auto_sign", false);
